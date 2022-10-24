040e0e1d87da0eff77e60709805674595ee4b4c9
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-24 10:50:02 -0700

    product name validate start

--- js/src/controller/ProductController.js
+++ js/src/controller/ProductController.js
@@ -9,6 +9,16 @@
 
 // product inputs validate
 
+    // product name
+
+
+
+
+
+
+
+
+
 
 $("#add_product_btn").click(function (event) {
 //  productSave($('#next_product_id').text(), $('#product_name').val(), $('#product_category').val(), $('#product_qty').val(), $('#product_price').val());
