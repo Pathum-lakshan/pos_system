342a21058704e612b18ddcebd04e950a6416e99e
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-24 05:09:59 -0700

    it not work try another

--- js/src/controller/ProductController.js
+++ js/src/controller/ProductController.js
@@ -9,21 +9,20 @@
 $("#add_product_btn").click(function (event) {
 //  productSave($('#next_product_id').text(), $('#product_name').val(), $('#product_category').val(), $('#product_qty').val(), $('#product_price').val());
 
-  console.log(validateFunction( /^[A-z ]{5,20}$/,$('#product_name').val()));
+  //console.log(validateFunction( /^[A-z ]{5,20}$/,));
 
-  function validateResourceName() {
     //get posted resource name value
-    var inputString = document.getElementById("resourceName").value;
+    const inputString = $('#product_name').val();
     //should be in the word\word\word format
-    var pattern=/[a-Z|/\\/|a-Z|/\\/|a-Z\s]/;
-
+    const pattern =/^ [0 - 9]{0,10}$/;
+console.log(pattern.test(inputString))
     //If the inputString is NOT a match
     if (!pattern.test(inputString)) {
-      alert("not a match");
+     console.log("kk")
     }
     else
     {
-      alert("match");
+console.log("ss")
     }
-  }
+
 });
