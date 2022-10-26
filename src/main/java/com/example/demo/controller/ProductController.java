878bf32008f1bc54cfb81474c0b71aa370b77c8a
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-26 10:17:43 +0530

    route created

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ALE IS TER
+ * 10/26/2022
+ * 10:17 AM
+ * pos_system
+ * com.example.demo.controller
+ * ProductController
+ */
+
+@RestController
+@RequestMapping(value = "api/v1/product")
+@CrossOrigin
+public class ProductController {
+
+}
