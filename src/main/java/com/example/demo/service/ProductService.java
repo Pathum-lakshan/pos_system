2380a98b695a360ee86b37363530056b29d309ae
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-26 23:43:46 +0530

    product repo inject to ProductService

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,5 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.repo.ProductRepo;
+import com.example.demo.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
 /**
  * ALE IS TER
  * 10/26/2022
@@ -8,5 +16,11 @@ package com.example.demo.service;
  * com.example.demo.service
  * ProductService
  */
+@Service
+@Transactional
 public class ProductService {
+  @Autowired
+  private ProductRepo productRepo;
+  @Autowired
+  private ModelMapper modelMapper;
 }
