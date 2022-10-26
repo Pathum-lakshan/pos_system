cb3e5e52faf61fdf9fd613fee13f71bfbcdd579b
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-26 10:17:43 +0530

    create ProductController.java

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+/**
+ * ALE IS TER
+ * 10/26/2022
+ * 10:17 AM
+ * pos_system
+ * com.example.demo.controller
+ * ProductController
+ */
+public class ProductController {
+
+}
