1f9bae826e35151191a5be0c821f1f20adb346e7
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-26 23:40:11 +0530

    create ProductService.java

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+/**
+ * ALE IS TER
+ * 10/26/2022
+ * 11:39 PM
+ * pos_system
+ * com.example.demo.service
+ * ProductService
+ */
+public class ProductService {
+}
